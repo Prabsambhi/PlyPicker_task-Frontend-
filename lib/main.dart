40af62db71df64cae0848933147ff097fc3d479c
Prabsambhi <prabsambhi@gmail.com>
2024-02-12 23:03:27 +0530

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:imagefrontend/home.dart';
 import 'package:imagefrontend/imageprovider.dart';
@@ -6,7 +7,9 @@ import 'package:imagefrontend/savedimage.dart';
 import 'package:imagefrontend/signup.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     MultiProvider(
       providers: [
